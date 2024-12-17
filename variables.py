de1011d0ffdd19209ccda8ae2e6dcb6d3e2605d2
Cortna39 <arthurishishe@gmail.com>
2024-12-17 20:48:27 +0400

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+dz_count = 12
+hours_spent = 1.5
+course_name = 'Python'
+time_4_task = 1.5/12
+print(f"Курс: {course_name}, всего задач:{dz_count}, затрачено часов: {hours_spent}, среднее время выполнения {time_4_task} часа.")
